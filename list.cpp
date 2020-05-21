79868babe95e8bd20a656bd6f2b8ad36c525c0ce
Hokokok <leemoo1004@gmail.com>
2020-05-21 18:19:12 +0900

    1차 구현 완료
    Node, LinkedList클래스 정의, 멤버함수 구현

---
+++ list.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "list.h"
+
+LinkedList::LinkedList()
+{
+    head = new Node();
+    head -> data = 0;
+    head -> next = nullptr;
+}
+
+LinkedList::~LinkedList()
+{
+    Node * cur = head;
+    while(cur != nullptr)
+    {
+        Node * tmp = cur -> next;
+        delete cur;
+        cur = tmp;
+    }
+}
+
+void LinkedList::Append(int data)
+{
+    Node * new_node = new Node();
+    new_node -> data = data;
+    new_node -> next = nullptr;
+
+    head -> next = new_node;
+}
+
+int LinkedList::Length()
+{
+    int l = 0;
+    Node * cur = head;
+    while(cur -> next != nullptr)
+    {
+        l++;
+        cur = cur -> next;
+    }
+}
+
+void LinkedList::Print()
+{
+    Node * cur = head;
+    while(cur -> next != nullptr)
+    {
+        std::cout << cur -> next -> data << " ";
+        cur = cur -> next;
+    }
+}
---
+++ list.h
@@ -0,0 +1,27 @@
+#ifndef _HOKOKOK_LIST_
+#define _HOKOKOK_LIST_ 1
+
+class LinkedList;
+
+class Node
+{
+private:
+    int data;
+    Node * next;
+public:
+    friend LinkedList;
+};
+
+class LinkedList
+{
+private:
+    Node * head;
+public:
+    LinkedList();
+    ~LinkedList();
+    void Append(int data);
+    int Length();
+    void Print();
+};
+
+#endif
